2df7af91352d25c7b18af872348e548876c9b422
Bernard Legaspi <blegaspi26@gmail.com>
2022-09-20 21:57:30 -0400

    commit tree 3531808fb8cbfaff956fde35592bbfea27cdb825

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+// first test
+//gives cutomer a free sample if ride is less than or equal to 400ft
+//logs : 'This one is on me '
+
+function scuberGreetingForFeet(value) {
+  if (value <= 400) {
+    return 'This one is on me!';
+  } else if (value >= 400 && value <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (value >= 2001 && value <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(amt) {
   // Write your code here!
+  switch (amt) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
